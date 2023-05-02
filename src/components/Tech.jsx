0cfaedf695ff0fb774d41f1f1a375a52e37194b0
lea-chefson <leac41@hotmail.com>
2023-05-02 21:58:45 +0700

    hide tech on mobilee

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,12 +1,30 @@
 import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants"; 
+import { useEffect, useState } from 'react';
 
 const Tech = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia(`(max-width: 500px)`);
+
+    setIsMobile(mediaQuery.matches);
+
+    const handleMediaQuerychange = (event) => {
+      setIsMobile(event.matches);
+    }
+
+    mediaQuery.addEventListener(`change`, handleMediaQuerychange);
+
+    return () => {
+      mediaQuery.removeEventListener(`change`, handleMediaQuerychange);
+    }
+   }, []);
   return (
     <div className="flex flex-row flex-wrap justify-center gap-10">
       {technologies.map((technology) => (
-        <div className="w-28 h-28" key={technology.name}>
+         <div className={`${isMobile ? "invisible absolute" : "opacity-100"} w-28 h-28`} key={technology.name}>
           <BallCanvas icon={technology.icon} />
         </div>
       ))}
